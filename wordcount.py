c742a0a75fffcf124bce05beefe7c449a8d7384d
Hackbright Student <no-reply@hackbrightacademy.com>
2018-04-09 12:21:17 -0700

    completed writing word count function

--- wordcount.py
+++ wordcount.py
@@ -4,15 +4,15 @@ def word_count(file):
     Opens specified file, iterates through line then words in the file and returns 
     a dictionary with words as keys and counts as values.
     """
-    with open(file) as input:
+    with open(file) as inputs:
         count_words = {}
 
-        for line in input:
+        for line in inputs:
             words = line.rstrip().split(' ')
 
             for word in words:
-                word.rstrip()
                 count_words[word] = count_words.get(word, 0) + 1
+
         return count_words
 
 
